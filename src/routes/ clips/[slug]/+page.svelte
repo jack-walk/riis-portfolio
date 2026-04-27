413aea365dd7c7f8be3447e4d3977bfd3aacdacf
jack-walk <jacktmwalker@gmail.com>
2026-04-27 19:56:00 -0400

    Adding cards

---
+++ src/routes/ clips/[slug]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Kicker from '$lib/components/Article/Kicker.svelte';
+  import Headline from '$lib/components/Article/Headline.svelte';
+  import Image from '$lib/components/Media/Image.svelte';
+  import { base } from '$app/paths';
+
+  let { data } = $props();
+</script>
+
+<div class="container">
+  <Kicker text="{data.profile.name}'s Portfolio" href="{base}/" />
+  <Headline text={data.clip.title} />
+
+  <Image src={data.clip.image} alt={data.clip.title} />
+
+  <p>{data.clip.description}</p>
+</div>
